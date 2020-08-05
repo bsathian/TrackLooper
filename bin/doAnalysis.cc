ffae99a18b0430a900d35c00490190f59eeb2603
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2020-08-04 17:23:40 -0700

    interface with new framework

--- bin/doAnalysis.cc
+++ bin/doAnalysis.cc
@@ -569,12 +569,7 @@ int main(int argc, char** argv)
 
                 // Takes two arguments, SDL::Hit, and detId
                 // SDL::Event internally will structure whether we already have the module instance or we need to create a new one.
-                event.addHitToModule(
-                        // a hit
-                        SDL::Hit(trk.ph2_x()[ihit], trk.ph2_y()[ihit], trk.ph2_z()[ihit], ihit),
-                        // add to module with "detId"
-                        trk.ph2_detId()[ihit]
-                        );
+                event.addHitToEvent(trk.ph2_x()[ihit], trk.ph2_y()[ihit], trk.ph2_z()[ihit],trk.ph2_detId()[ihit]);
 
             }
 
