f504d53ae651029478e31c2d2bb52f01f44a9f7d
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2020-10-11 16:24:01 -0700

    return of trkcore for studies

---
+++ src/trkCore.cc
@@ -0,0 +1,406 @@
+#include "trkCore.h"
+
+//__________________________________________________________________________________________
+float simhit_p(unsigned int simhitidx)
+{
+
+    // |momentum| calculation
+
+    float px = trk.simhit_px()[simhitidx];
+    float py = trk.simhit_py()[simhitidx];
+    float pz = trk.simhit_pz()[simhitidx];
+    return sqrt(px*px + py*py + pz*pz);
+}
+
+//__________________________________________________________________________________________
+float hitAngle(unsigned int simhitidx)
+{
+
+    // This is the angle calculation between position vector and the momentum vector
+
+    float x = trk.simhit_x()[simhitidx];
+    float y = trk.simhit_y()[simhitidx];
+    float z = trk.simhit_z()[simhitidx];
+    float r3 = sqrt(x*x + y*y + z*z);
+    float px = trk.simhit_px()[simhitidx];
+    float py = trk.simhit_py()[simhitidx];
+    float pz = trk.simhit_pz()[simhitidx];
+    float p = sqrt(px*px + py*py + pz*pz);
+    float rdotp = x*px + y*py + z*pz;
+    rdotp = rdotp / r3;
+    rdotp = rdotp / p;
+    float angle = acos(rdotp);
+    return angle;
+}
+
+//__________________________________________________________________________________________
+bool isMuonCurlingHit(unsigned int isimtrk, unsigned int ith_hit)
+{
+
+    // To assess whether the ith_hit for isimtrk is a "curling" hit
+
+    // Retrieve the sim hit idx
+    unsigned int simhitidx = trk.sim_simHitIdx()[isimtrk][ith_hit];
+
+    // We're only concerned about hits in the outer tracker
+    // This is more of a sanity check
+    if (not (trk.simhit_subdet()[simhitidx] == 4 or trk.simhit_subdet()[simhitidx] == 5))
+        return false;
+
+    // Retrieve the sim hit pdgId
+    int simhit_particle = trk.simhit_particle()[simhitidx];
+
+    // If the hit is not muon then we can't tell anything
+    if (abs(simhit_particle) != 13)
+        return false;
+
+    // Get the angle
+    float angle = hitAngle(simhitidx);
+
+    // If the angle is quite different then it's the last hit
+    if (abs(angle) > 1.6)
+        return true;
+
+    // Afterwards, we check the energy loss
+    //
+    // If it is the first hit without any previous hit present,
+    // we can't tell whether it is last hit or not
+    // so we just say false to be conservative
+    if (ith_hit == 0)
+        return false;
+
+    // Retrieve the module where the hit lies
+    int detid = trk.simhit_detId()[simhitidx];
+    SDL::Module module = SDL::Module(detid);
+
+    // Calculate the momentum
+    float p = simhit_p(simhitidx);
+
+    // Find the previous simhit that is on the lower side of the module
+    int simhitidx_previous = -999;
+    for (unsigned int ith_back = 1; ith_back <= ith_hit; ith_back++)
+    {
+        // Retrieve the hit idx of ith_hit - ith_back;
+        unsigned int simhitidx_previous_candidate = trk.sim_simHitIdx()[isimtrk][ith_hit-ith_back];
+
+        if (not (trk.simhit_subdet()[simhitidx_previous_candidate] == 4 or trk.simhit_subdet()[simhitidx_previous_candidate] == 5))
+            continue;
+
+        if (not (trk.simhit_particle()[simhitidx_previous_candidate] == 13))
+            continue;
+
+        // Retrieve the module where the previous candidate hit lies
+        int detid = trk.simhit_detId()[simhitidx_previous_candidate];
+        SDL::Module module = SDL::Module(detid);
+
+        // If the module is lower, then we get the index
+        if (module.isLower())
+        {
+            simhitidx_previous = simhitidx_previous_candidate;
+            break;
+        }
+
+    }
+
+    // If no previous layer is found then can't do much
+    if (simhitidx_previous == -999)
+        return false;
+
+    // Get the previous layer momentum
+    float p_previous = simhit_p(simhitidx_previous);
+
+    // Calculate the momentum loss
+    float loss = fabs(p_previous - p) / p_previous;
+
+    // If the momentum loss is large it is the last hit
+    if (loss > 0.35)
+        return true;
+
+    // If it reaches this point again, we're not sure what this hit is
+    // So we return false 
+    return false;
+
+}
+
+
+
+
+bool hasAll12HitsWithNBarrelUsingModuleMap(unsigned int isimtrk, int nbarrel, bool usesimhits)
+{
+
+    // Select only tracks that left all 12 hits in the barrel
+    std::array<std::vector<SDL::Module>, 6> layers_modules_barrel; // Watch out for duplicates in this vector, do not count with this for unique count.
+    std::array<std::vector<SDL::Module>, 6> layers_modules_endcap; // Watch out for duplicates in this vector, do not count with this for unique count.
+
+    std::vector<float> ps;
+
+    for (unsigned int ith_hit = 0; ith_hit < trk.sim_simHitIdx()[isimtrk].size(); ++ith_hit)
+    {
+
+        // Retrieve the sim hit idx
+        unsigned int simhitidx = trk.sim_simHitIdx()[isimtrk][ith_hit];
+
+        // Select only the hits in the outer tracker
+        if (not (trk.simhit_subdet()[simhitidx] == 4 or trk.simhit_subdet()[simhitidx] == 5))
+            continue;
+
+        if (not (trk.simhit_particle()[simhitidx] == trk.sim_pdgId()[isimtrk]))
+            continue;
+
+        if (isMuonCurlingHit(isimtrk, ith_hit))
+            break;
+
+        if (not usesimhits)
+        {
+
+            // list of reco hit matched to this sim hit
+            for (unsigned int irecohit = 0; irecohit < trk.simhit_hitIdx()[simhitidx].size(); ++irecohit)
+            {
+                // Get the recohit type
+                int recohittype = trk.simhit_hitType()[simhitidx][irecohit];
+
+                // Consider only ph2 hits (i.e. outer tracker hits)
+                if (recohittype == 4)
+                {
+
+                    int ihit = trk.simhit_hitIdx()[simhitidx][irecohit];
+
+                    if (trk.ph2_subdet()[ihit] == 5)
+                    {
+                        layers_modules_barrel[trk.ph2_layer()[ihit] - 1].push_back(SDL::Module(trk.ph2_detId()[ihit]));
+                    }
+                    if (trk.ph2_subdet()[ihit] == 4)
+                    {
+                        layers_modules_endcap[trk.ph2_layer()[ihit] - 1].push_back(SDL::Module(trk.ph2_detId()[ihit]));
+                    }
+
+                }
+
+            }
+
+        }
+        else
+        {
+
+            if (trk.simhit_hitIdx()[simhitidx].size() > 0)
+            {
+
+                if (trk.simhit_subdet()[simhitidx] == 5)
+                {
+                    layers_modules_barrel[trk.simhit_layer()[simhitidx] - 1].push_back(SDL::Module(trk.simhit_detId()[simhitidx]));
+                }
+                if (trk.simhit_subdet()[simhitidx] == 4)
+                {
+                    layers_modules_endcap[trk.simhit_layer()[simhitidx] - 1].push_back(SDL::Module(trk.simhit_detId()[simhitidx]));
+                }
+            }
+
+            // // list of reco hit matched to this sim hit
+            // for (unsigned int irecohit = 0; irecohit < trk.simhit_hitIdx()[simhitidx].size(); ++irecohit)
+            // {
+            //     // Get the recohit type
+            //     int recohittype = trk.simhit_hitType()[simhitidx][irecohit];
+
+            //     // Consider only ph2 hits (i.e. outer tracker hits)
+            //     if (recohittype == 4)
+            //     {
+
+            //         int ihit = trk.simhit_hitIdx()[simhitidx][irecohit];
+
+            //         if (trk.ph2_subdet()[ihit] == 5)
+            //         {
+            //             layers_modules_barrel[trk.ph2_layer()[ihit] - 1].push_back(SDL::Module(trk.ph2_detId()[ihit]));
+            //         }
+            //         if (trk.ph2_subdet()[ihit] == 4)
+            //         {
+            //             layers_modules_endcap[trk.ph2_layer()[ihit] - 1].push_back(SDL::Module(trk.ph2_detId()[ihit]));
+            //         }
+
+            //     }
+
+            // }
+
+        }
+
+    }
+
+    // Aggregating good pair modules (i.e. a double module with each side having non-zero reco hits.)
+    std::array<std::vector<unsigned int>, 6> layers_good_paired_modules; // Watch out for duplicates in this vector, do not count with this for unique count.
+
+    for (int logical_ilayer = 0; logical_ilayer < 6; ++logical_ilayer)
+    {
+        // Raw layer number
+        bool is_ilayer_barrel = logical_ilayer < nbarrel;
+        int raw_ilayer = is_ilayer_barrel ? logical_ilayer: logical_ilayer - nbarrel;
+        const std::array<std::vector<SDL::Module>, 6>& layers_modules = is_ilayer_barrel ? layers_modules_barrel : layers_modules_endcap;
+
+        // Then get the module in the ilayer and check that it has a good module pair
+        // Loop over modules in the given raw_ilayer and match the pairs and if a good pair of modules have hits in each module
+        // then save the lower modules to layers_good_paired_modules.
+        // NOTE there may be duplicates being pushed to layers_good_paired_modules
+        // Do not count with these vectors
+        for (unsigned imod = 0; imod < layers_modules[raw_ilayer].size(); ++imod)
+        {
+            for (unsigned jmod = imod + 1; jmod < layers_modules[raw_ilayer].size(); ++jmod)
+            {
+                // if two modules are a good pair
+                if (layers_modules[raw_ilayer][imod].partnerDetId() == layers_modules[raw_ilayer][jmod].detId())
+                {
+                    // add the lower module one to the good_paired_modules list
+                    if (layers_modules[raw_ilayer][imod].isLower())
+                    {
+                        if (std::find(
+                                    layers_good_paired_modules[logical_ilayer].begin(),
+                                    layers_good_paired_modules[logical_ilayer].end(),
+                                    layers_modules[raw_ilayer][imod].detId()) == layers_good_paired_modules[logical_ilayer].end())
+                            layers_good_paired_modules[logical_ilayer].push_back(layers_modules[raw_ilayer][imod].detId());
+                    }
+                    else
+                    {
+                        if (std::find(
+                                    layers_good_paired_modules[logical_ilayer].begin(),
+                                    layers_good_paired_modules[logical_ilayer].end(),
+                                    layers_modules[raw_ilayer][imod].partnerDetId()) == layers_good_paired_modules[logical_ilayer].end())
+                        layers_good_paired_modules[logical_ilayer].push_back(layers_modules[raw_ilayer][imod].partnerDetId());
+                    }
+                }
+            }
+        }
+    }
+
+    return checkModuleConnectionsAreGood(layers_good_paired_modules);
+
+}
+
+
+
+bool hasAll12HitsWithNBarrel(unsigned int isimtrk, int nbarrel)
+{
+
+    // Select only tracks that left all 12 hits in the barrel
+    std::array<std::vector<SDL::Module>, 6> layers_modules_barrel;
+    std::array<std::vector<SDL::Module>, 6> layers_modules_endcap;
+
+    std::vector<float> ps;
+
+    for (unsigned int ith_hit = 0; ith_hit < trk.sim_simHitIdx()[isimtrk].size(); ++ith_hit)
+    {
+
+        // Retrieve the sim hit idx
+        unsigned int simhitidx = trk.sim_simHitIdx()[isimtrk][ith_hit];
+
+        // Select only the hits in the outer tracker
+        if (not (trk.simhit_subdet()[simhitidx] == 4 or trk.simhit_subdet()[simhitidx] == 5))
+            continue;
+
+        // if (not (trk.simhit_particle()[simhitidx] == trk.sim_pdgId()[isimtrk]))
+        //     continue;
+
+        if (isMuonCurlingHit(isimtrk, ith_hit))
+            break;
+
+        // list of reco hit matched to this sim hit
+        for (unsigned int irecohit = 0; irecohit < trk.simhit_hitIdx()[simhitidx].size(); ++irecohit)
+        {
+            // Get the recohit type
+            int recohittype = trk.simhit_hitType()[simhitidx][irecohit];
+
+            // Consider only ph2 hits (i.e. outer tracker hits)
+            if (recohittype == 4)
+            {
+
+                int ihit = trk.simhit_hitIdx()[simhitidx][irecohit];
+
+                if (trk.ph2_subdet()[ihit] == 5)
+                {
+                    layers_modules_barrel[trk.ph2_layer()[ihit] - 1].push_back(SDL::Module(trk.ph2_detId()[ihit]));
+                }
+                if (trk.ph2_subdet()[ihit] == 4)
+                {
+                    layers_modules_endcap[trk.ph2_layer()[ihit] - 1].push_back(SDL::Module(trk.ph2_detId()[ihit]));
+                }
+
+            }
+
+        }
+
+    }
+
+    std::array<bool, 6> has_good_pair_by_layer_barrel;
+    has_good_pair_by_layer_barrel[0] = false;
+    has_good_pair_by_layer_barrel[1] = false;
+    has_good_pair_by_layer_barrel[2] = false;
+    has_good_pair_by_layer_barrel[3] = false;
+    has_good_pair_by_layer_barrel[4] = false;
+    has_good_pair_by_layer_barrel[5] = false;
+
+    std::array<bool, 6> has_good_pair_by_layer_endcap;
+    has_good_pair_by_layer_endcap[0] = false;
+    has_good_pair_by_layer_endcap[1] = false;
+    has_good_pair_by_layer_endcap[2] = false;
+    has_good_pair_by_layer_endcap[3] = false;
+    has_good_pair_by_layer_endcap[4] = false;
+    has_good_pair_by_layer_endcap[5] = false;
+
+    bool has_good_pair_all_layer = true;
+
+    for (int ilayer = 0; ilayer < 6; ++ilayer)
+    {
+
+        bool has_good_pair = false;
+
+        if (ilayer < nbarrel)
+        {
+
+            for (unsigned imod = 0; imod < layers_modules_barrel[ilayer].size(); ++imod)
+            {
+                for (unsigned jmod = imod + 1; jmod < layers_modules_barrel[ilayer].size(); ++jmod)
+                {
+                    if (layers_modules_barrel[ilayer][imod].partnerDetId() == layers_modules_barrel[ilayer][jmod].detId())
+                        has_good_pair = true;
+                }
+            }
+
+        }
+        else
+        {
+
+            int endcap_ilayer = ilayer - nbarrel;
+
+            for (unsigned imod = 0; imod < layers_modules_endcap[endcap_ilayer].size(); ++imod)
+            {
+                for (unsigned jmod = imod + 1; jmod < layers_modules_endcap[endcap_ilayer].size(); ++jmod)
+                {
+                    if (layers_modules_endcap[endcap_ilayer][imod].partnerDetId() == layers_modules_endcap[endcap_ilayer][jmod].detId())
+                        has_good_pair = true;
+                }
+            }
+
+        }
+
+        if (not has_good_pair)
+            has_good_pair_all_layer = false;
+
+        if (ilayer < nbarrel)
+            has_good_pair_by_layer_barrel[ilayer] = has_good_pair;
+        else
+            has_good_pair_by_layer_endcap[ilayer] = has_good_pair;
+
+    }
+
+
+    float pt = trk.sim_pt()[isimtrk];
+    float eta = trk.sim_eta()[isimtrk];
+
+    // if (abs((trk.sim_pt()[isimtrk] - 0.71710)) < 0.00001)
+    // {
+    //     std::cout << std::endl;
+    //     std::cout <<  " has_good_pair_by_layer[0]: " << has_good_pair_by_layer[0] <<  " has_good_pair_by_layer[1]: " << has_good_pair_by_layer[1] <<  " has_good_pair_by_layer[2]: " << has_good_pair_by_layer[2] <<  " has_good_pair_by_layer[3]: " << has_good_pair_by_layer[3] <<  " has_good_pair_by_layer[4]: " << has_good_pair_by_layer[4] <<  " has_good_pair_by_layer[5]: " << has_good_pair_by_layer[5] <<  " pt: " << pt <<  " eta: " << eta <<  std::endl;
+    // }
+
+    return has_good_pair_all_layer;
+
+}
+
+
+
---
+++ src/trkCore.h
@@ -0,0 +1,33 @@
+#ifndef trkCore_h
+#define trkCore_h
+
+#include "trktree.h"
+#include "SDL/Module.h" // SDL::Module
+#include "TCanvas.h"
+#include "AnalysisInterface/EventForAnalysisInterface.h"
+#include "AnalysisInterface/Module.h"
+#include "SDL/ModuleConnectionMap.h"
+
+enum TrackletType {
+    BB1BB3 = 0,
+    BB2BB4,
+    BB3BB5,
+    BB1BE3,
+    BB2BE4,
+    BB3BE5,
+    BB1EE3,
+    BB2EE4,
+    BB3EE5,
+    BE1EE3,
+    BE2EE4,
+    BE3EE5,
+    EE1EE3,
+    EE2EE4,
+};
+
+float simhit_p(unsigned int simhitidx);
+float hitAngle(unsigned int simhitidx);
+bool isMuonCurlingHit(unsigned int isimtrk, unsigned int ith_hit);
+bool hasAll12HitsWithNBarrel(unsigned int isimtrk, int nbarrel);
+bool hasAll12HitsWithNBarrelUsingModuleMap(unsigned int isimtrk, int nbarrel, bool usesimhits=false);
+#endif
