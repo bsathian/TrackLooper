ea3965070509617903d8bed90fcf30512fcd9eb7
Balaji Sathia Narayanan <bsathian@phi3.t2.ucsd.edu>
2020-11-10 15:02:16 -0800

    changes

--- bin/doAnalysis.h
+++ bin/doAnalysis.h
@@ -14,12 +14,18 @@
 #include "SDL/Module.cuh" // SDL::Module
 #include "SDL/EndcapGeometry.h" // SDL::EndcapGeometry
 #include "SDL/ModuleConnectionMap.h" // SDL::ModuleConnectionMap
+#include <cppitertools/itertools.hpp>
 
 // Efficiency study modules
 #include "Study.h"
 
 #include "StudyTrackletOccupancy.h"
 #include "StudyTripletOccupancy.h"
+#include "StudySegmentOccupancy.h"
+#include "StudyTrackCandidateOccupancy.h"
+#include "StudyTrackCandidateOccupancyT4T4.h"
+#include "StudyTrackCandidateOccupancyT4T3.h"
+#include "StudyTrackCandidateOccupancyT3T4.h"
 
 #include "constants.h"
 
