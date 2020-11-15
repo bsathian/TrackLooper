8261f3ee8ab284b9b054dccdb24e5dd0d3b8d509
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2020-11-15 14:40:44 -0800

    more changes

--- src/AnalysisInterface/EventForAnalysisInterface.cc
+++ src/AnalysisInterface/EventForAnalysisInterface.cc
@@ -71,7 +71,7 @@ void SDL::EventForAnalysisInterface::addMDsToAnalysisInterface(struct miniDouble
             Hit* lowerHitPtr = hits_[mdsInGPU.hitIndices[2 * mdIndex]];
             Hit* upperHitPtr = hits_[mdsInGPU.hitIndices[2 * mdIndex + 1]];
 
-            miniDoublets_[mdIndex] = new SDL::MiniDoublet(mdsInGPU.dzs[mdIndex], mdsInGPU.dphis[mdIndex], mdsInGPU.dphichanges[mdIndex], mdsInGPU.noShiftedDphis[mdIndex], mdsInGPU.noShiftedDphiChanges[mdIndex], lowerHitPtr, upperHitPtr);
+            miniDoublets_[mdIndex] = new SDL::MiniDoublet(mdsInGPU.dzs[mdIndex], mdsInGPU.drts[index], mdsInGPU.dphis[mdIndex], mdsInGPU.dphichanges[mdIndex], mdsInGPU.noShiftedDphis[mdIndex], mdsInGPU.noShiftedDphiChanges[mdIndex], mdsInGPU.dzCuts[index], mdsInGPU.drtCuts[index], mdsInGPU.miniCuts[index], lowerHitPtr, upperHitPtr);
 
             mdPointers.push_back(miniDoublets_[mdIndex]);
             Module& lowerModule = lowerHitPtr->getModule();
